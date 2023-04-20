17efd723a0f53b9d87f333e4c1bf5876f4224e46
L.J <109549349+JuraszekL@users.noreply.github.com>
2023-04-20 21:07:17 +0200

    Created 3 driver's files

---
+++ bme280.h
@@ -0,0 +1,37 @@
+/*
+ * This is a non-commercial project for learning purposes only. Feel free to use it.
+ *
+ * JuraszekL
+ *
+ * */
+
+//***************************************
+
+#ifndef BME280_H
+#define BME280_H
+
+//***************************************
+
+#ifdef __cplusplus /* CPP */
+extern "C" {
+#endif
+
+//***************************************
+
+#include "bme280_definitions.h"
+
+//***************************************
+
+
+
+
+
+//***************************************
+
+#ifdef __cplusplus
+}
+#endif /* CPP */
+
+//***************************************
+
+#endif /* BME280_H */
---
+++ bme280_definitions.h
@@ -0,0 +1,19 @@
+/*
+ * This is a non-commercial project for learning purposes only. Feel free to use it.
+ *
+ * JuraszekL
+ *
+ * */
+
+//***************************************
+
+#ifndef BME280_DEFINITIONS_H
+#define BME280_DEFINITIONS_H
+
+//***************************************
+
+
+
+//***************************************
+
+#endif /* BME280_DEFINITIONS_H */
