9231b1d9b879ae11a8d27bd92b272dcd5b94d095
L.J <109549349+JuraszekL@users.noreply.github.com>
2023-04-20 22:17:48 +0200

    interface typedef + part of a device struct

--- bme280_definitions.h
+++ bme280_definitions.h
@@ -12,8 +12,51 @@
 
 //***************************************
 
-
+#include <stdint.h>
+#include <stddef.h>
 
 //***************************************
+/* returned values */
+//***************************************
+
+#define BME280_OK	(0)
+#define BME280_ERR	(-1)
+
+//***************************************
+/* I2C address */
+//***************************************
+
+// if SDO pin is connected to GND
+#define BME280_I2CADDR_SDOL	(0x76)
+// if SDO pin is connected to VCC
+#define BME280_I2CADDR_SDOH	(0x77)
+
+//***************************************
+/* registers values */
+//***************************************
+
+#define BME280_ID	(0x60)
+
+//***************************************
+/* registers values */
+//***************************************
+
+typedef enum {
+
+	i2c = 1U,
+	spi
+
+}BME280_InterfaceType_t;
+
+//***************************************
+/* device structure */
+//***************************************
+
+typedef struct {
+
+	BME280_InterfaceType_t interface;
+	uint8_t i2c_address;
+
+}BME280_Device_t;
 
 #endif /* BME280_DEFINITIONS_H */
