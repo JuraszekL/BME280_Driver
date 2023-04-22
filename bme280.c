c129ee20944837abb35e19436757dc37e45b8a4d
L.J <109549349+JuraszekL@users.noreply.github.com>
2023-04-22 22:41:33 +0200

    new definitions of sensor parameters + code cleanup

--- bme280.c
+++ bme280.c
@@ -43,7 +43,7 @@ int8_t BME280_Init(BME280_t *Dev, uint8_t I2cAddr, void *EnvSpecData,
 	Dev->write = WriteFun;
 
 	/* read and check chip ID */
-	res = Dev->read(BME280_REGADDR_ID, &id, 1, Dev->i2c_address, Dev->env_spec_data);
+	res = Dev->read(BME280_ID_ADDR, &id, 1, Dev->i2c_address, Dev->env_spec_data);
 	if(BME280_OK != res) return BME280_INTERFACE_ERR;
 
 	if(BME280_ID != id) return BME280_ID_ERROR;
--- bme280_definitions.h
+++ bme280_definitions.h
@@ -38,13 +38,47 @@
 
 	/* ID related */
 #define BME280_ID			(0x60)
-#define BME280_REGADDR_ID	(0xD0)
+#define BME280_ID_ADDR		(0xD0)
 
 	/* calibration data related */
 #define BME280_CALIB_DATA1_ADDR	(0x88)
 #define BME280_CALIB_DATA1_LEN	(25)
 #define BME280_CALIB_DATA2_ADDR	(0xE1)
 #define BME280_CALIB_DATA2_LEN	(7)
+
+//***************************************
+/* settings */
+//***************************************
+
+	/* oversampling values */
+#define BME280_OVERSAMPLING_SKIPP	(0x00)
+#define BME280_OVERSAMPLING_X1		(0x01)
+#define BME280_OVERSAMPLING_X2		(0x02)
+#define BME280_OVERSAMPLING_X4		(0x03)
+#define BME280_OVERSAMPLING_X8		(0x04)
+#define BME280_OVERSAMPLING_X16		(0x05)
+
+	/* operating mode */
+#define BME280_SLEEPMODE	(0x00)
+#define BME280_FORCEDMODE	(0x01)
+#define BME280_NORMALMODE	(0x03)
+
+	/* standby time in normal mode  */
+#define BME280_STBY_0_5MS	(0x00)
+#define BME280_STBY_62_5MS	(0x01)
+#define BME280_STBY_125MS	(0x02)
+#define BME280_STBY_250MS	(0x03)
+#define BME280_STBY_500MS	(0x04)
+#define BME280_STBY_1000MS	(0x05)
+#define BME280_STBY_10MS	(0x06)
+#define BME280_STBY_20MS	(0x07)
+
+	/* filter coefficient */
+#define BME280_FILTER_OFF	(0x00)
+#define BME280_FILTER_2		(0x01)
+#define BME280_FILTER_4		(0x02)
+#define BME280_FILTER_8		(0x03)
+#define BME280_FILTER_16	(0x04)
 
 //***************************************
 /* macros */
@@ -59,13 +93,13 @@
 /* typedefs */
 //***************************************
 
-//typedef enum {bme280_i2c = 1U, bme280_spi} BME280_InterfaceType_t;
-
+	/* pointers prototypes to user-defined and platform-dependent functions to
+	 * comminucate with sensor */
 typedef int8_t (*bme280_readbytes)(uint8_t reg_addr, uint8_t *rxbuff, uint8_t rxlen, uint8_t dev_addr, void *env_spec_data);
 typedef int8_t (*bme280_writebyte)(uint8_t reg_addr, uint8_t value, uint8_t dev_addr, void *env_spec_data);
 
+	/* main structure where single sensor is cofigured */
 typedef struct BME280_device BME280_t;
-
 
 struct BME280_calibration_data {
 
@@ -91,10 +125,8 @@ struct BME280_calibration_data {
 	int8_t dig_H6;
 };
 
-
 struct BME280_device {
 
-//	BME280_InterfaceType_t interface;
 	uint8_t i2c_address;
 	void *env_spec_data;
 
@@ -103,6 +135,5 @@ struct BME280_device {
 
 	struct BME280_calibration_data trimm;
 };
-
 
 #endif /* BME280_DEFINITIONS_H */
